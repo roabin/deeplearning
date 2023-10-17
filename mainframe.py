e0694e20d2105f1c94d15b17c7fe776c1de5b5ef
roabin <roabin@gmail.com>
2023-10-17 10:45:08 +0800

    2023.10.17

--- mainframe.py
+++ mainframe.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+'''
+MINIST: 28*28 -> 100 -> ReLu ->10 Linear 
+'''
 import pdb
 import torch
 from torch import nn
@@ -6,7 +9,7 @@ from torch import optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms.v2 import ToTensor
+from torchvision.transforms import ToTensor, Lambda
 
 dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 # device = (
@@ -23,10 +26,9 @@ class RubinNN(nn.Module):
 	"""docstring for RubinNN"nn.Module"""
 	def __init__(self):  #  define all layers
 		super(RubinNN,self).__init__()
-		self.flatten = nn.Flatten()
-		#归一。。view()
+			#归一。。view()
 		self.network = nn.Sequential(
-			nn.Flatten()
+			nn.Flatten(),
 			nn.Linear(28*28, 100),
 			nn.ReLU(),
 			nn.Linear(100,10),
@@ -45,34 +47,36 @@ class RubinNN(nn.Module):
 
 ## Training function
 
-def train(model, loss_fn, opt, train_dl):pass
-	# model.train()
-	# for xb, yb in train_dl:
-	# 	xb, yb = xb.to(dev), yb.to(dev)
-	# 	
-	# 	y = model(xb)
-	# 	loss = loss_fn(y, yb)
-	# 	loss.backward()
-	# 	opt.step()
-	# 	opt.zero_grad()
+def train(model, loss_fn, opt, train_dl):
+	model.train()
+	for batch, (xb, yb) in enumerate(train_dl):
+		xb, yb = xb.to(dev), yb.to(dev)		
+		y = model(xb)
+		loss = loss_fn(y, yb)
+		loss.backward()
+		opt.step()
+		opt.zero_grad()
+		pdb.set_trace()
+		if batch % 100 == 0:
+			loss = loss.item()
+			print(f"loss:{loss:>7f}")
 		
-def valid(model, loss_fn, valid_dl):pass
-	# batch_num = len(valid_dl)
-	# size = len(valid_dl.dataset)
-	# total_loss, correct = 0, 0
+def valid(model, loss_fn, valid_dl):
+	batch_num = len(valid_dl)
+	size = len(valid_dl.dataset)
+	total_loss, correct = 0, 0
 
-	# model.eval()
-	# with torch.no_grad():
-	# 	for xb, yb in valid_dl:
-	#		xb, yb = xb.to(dev), yb.to(dev)
-	# 		y = model(xb)
-	# 		loss = loss_fn(y, yb)
-	# 		total_loss += loss
-	# 		correct = 
-
-	# test_loss = total_loss/batch_num
-	# correct_rate = correct/size
-	# print(f"Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+	model.eval()
+	with torch.no_grad():
+		for xb, yb in valid_dl:
+			xb, yb = xb.to(dev), yb.to(dev)
+			y = model(xb)
+			loss = loss_fn(y, yb)
+			total_loss += loss
+			correct += (y.argmax(1) == yb).type(torch.float).sum().item()
+	test_loss = total_loss/batch_num
+	correct /= size
+	print(f"Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
 # training dataset prepration
@@ -80,18 +84,20 @@ train_ds = datasets.MNIST(
 	root = 'data',
 	train = True,
 	download = True,
-	transform = ToTensor()
+	transform = ToTensor(),
+	#target_transform = Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y), value=1))
 	)
 valid_ds = datasets.MNIST(
 	root = 'data',
 	train = False,
 	download = True,
-	transform = ToTensor()
+	transform = ToTensor(),
+	#target_transform = Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y), value=1))
 	)
 
 # HyperParameters settings
 lr = 1e-3
-epochs = 1
+epochs = 10
 bs = 64
 	            ## Begin of Training
 train_dl = DataLoader(train_ds, batch_size = bs, shuffle = True)
